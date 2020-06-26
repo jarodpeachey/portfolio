1561338d753b8ee300b81e1d59e0b3a597873fe3
Jarod Peachey <jwpeachey107@aol.com>
2020-06-26 18:59:47 -0400

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Jarod Peachey - Front End Web Developer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    defaultTitle: `Jarod Peachey - Front End Web Developer`,
+    defaultDescription: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    baseUrl: '/',
+    author: '@jarod_peachey',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/seo.js
+++ src/components/seo.js
@@ -6,83 +6,66 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
+import { useLocation } from '@reach/router';
 import { useStaticQuery, graphql } from 'gatsby';
 
-function SEO ({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `,
-  );
+const SEO = ({ title, description, image, article, slug }) => {
+  const { pathname } = useLocation();
+  const { site } = useStaticQuery(query);
 
-  const metaDescription = description || site.siteMetadata.description;
+  const {
+    defaultTitle,
+    defaultDescription,
+    baseUrl,
+    author,
+  } = site.siteMetadata;
+
+  // const defaultImage = '/images/home.png';
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    // image: `https://${baseUrl}${image}` || `https://${baseUrl}${defaultImage}`,
+    url: `https://jarodpeachey.netlify.app${pathname}`,
+  };
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: 'description',
-          content: metaDescription,
-        },
-        {
-          property: 'og:title',
-          content: title,
-        },
-        {
-          property: 'og:description',
-          content: metaDescription,
-        },
-        {
-          property: 'og:type',
-          content: 'website',
-        },
-        {
-          name: 'twitter:card',
-          content: 'summary',
-        },
-        {
-          name: 'twitter:creator',
-          content: site.siteMetadata.author,
-        },
-        {
-          name: 'twitter:title',
-          content: title,
-        },
-        {
-          name: 'twitter:description',
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
+    <Helmet title={seo.title}>
+      <html lang="en" />
+      <meta name="description" content={seo.description} />
+      {/* <meta name="image" content={seo.image} /> */}
+
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {(article ? true : null) && <meta property="og:type" content="article" />}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+      {/* {seo.image && <meta property="og:image" content={seo.image} />} */}
+
+      {/* <meta name="twitter:card" content="summary_large_image" /> */}
+      {author && <meta name="twitter:creator" content={author} />}
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.description && (
+        <meta name="twitter:description" content={seo.description} />
+      )}
+      {/* {seo.image && <meta name="twitter:image" content={seo.image} />} */}
+    </Helmet>
   );
-}
-
-SEO.defaultProps = {
-  lang: 'en',
-  meta: [],
-  description: '',
 };
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-};
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        defaultDescription: description
+        baseUrl
+        author
+      }
+    }
+  }
+`;
 
 export default SEO;
