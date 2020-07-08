309bb2a102ad11817b295a72c704c3e62a3180e5
Jarod Peachey <jwpeachey107@aol.com>
2020-07-08 18:47:40 -0400

    Fix image

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -48,7 +48,7 @@ const Blog = ({ pageContext }) => {
         title="Blog - Jarod Peachey"
         description="A collection of tips, tricks, bug fixes and random thoughts
           about web development. React, Javascript and the JAMstack."
-        image="/images/blog/seo.js"
+        image="/images/blog/seo.png"
       />
       <Hero
         background={`linear-gradient(
