19a4a6fb164997bf2dec96f520aefd56f81102f3
Jarod Peachey <jwpeachey107@aol.com>
2020-07-08 10:19:07 -0400

    Add another test

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -47,7 +47,7 @@ const SEO = ({ title, description, image, article, slug }) => {
 
       {/* <meta name="twitter:widgets:theme" content="light" />
       <meta name="twitter:dnt" content="on" />
-      <meta name="twitter:card" content="summary_large_image" />
+      // <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={author || '@jarod_peachey'} />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} /> */}
