545f363149ee7ce6babb4de1d23ea7bd611c1ac5
Jarod Peachey <jwpeachey107@aol.com>
2020-07-08 07:15:17 -0400

    Update SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -33,25 +33,24 @@ const SEO = ({ title, description, image, article, slug }) => {
   return (
     <Helmet title={seo.title}>
       <html lang="en" />
+      <link
+        rel="canonical"
+        href={`https://jarodpeachey.netlify.com${pathname}`}
+      />
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      <link rel="canonical" href={`https://jarodpeachey.netlify.com${pathname}`} />
 
-      {seo.url && <meta property="og:url" content={seo.url} />}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
-      {seo.title && <meta property="og:title" content={seo.title} />}
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
-      {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content={seo.url} />
+      <meta name="og:description" content={seo.description} />
+      <meta name="og:image" content={seo.image} />
 
+      <meta name="twitter:widgets:theme" content="light" />
+      <meta name="twitter:dnt" content="on" />
       <meta name="twitter:card" content="summary_large_image" />
-      {author && <meta name="twitter:creator" content={author} />}
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
+      <meta name="twitter:creator" content={author || '@jarod_peachey'} />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
     </Helmet>
   );
 };
